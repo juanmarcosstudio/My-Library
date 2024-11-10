3df56290c39670e8db0ba454d6f4359d217b5a93
Jhon Mark <jhonmarktamayo134@gmail.com>
2024-11-11 01:06:47 +0700

    add: display book and remove book function

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
-const myLibrary = []
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+  this.read = !this.read;
 };
 
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    displayBooks();
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayBooks();
 }
 
 function displayBooks() {
-    const libraryContainer = document.getElementById('library');
-    libraryContainer.innerHTML = '';
+  const libraryContainer = document.getElementById('library');
+  libraryContainer.innerHTML = '';
+  
+  myLibrary.forEach((book, index) => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
 
-    myLibrary.forEach((book, index) => {
-        const bookCard = document.createElement('div');
-        bookCard.classList.add('book-card');
+    bookCard.innerHTML = `
+      <h2>${book.title}</h2>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Read: ${book.read ? "Yes" : "No"}</p>
+      <button onclick="toggleReadStatus(${index})">Toggle Read Status</button>
+      <button onclick="removeBook(${index})">Remove Book</button>
+    `;
 
-        bookCard.innerHTML =`
-            <h2>${book.title}</h2>
-            <p>Author: ${book.author}</p>
-            <p>Pages: ${book.pages}</p>
-            <p>Read: ${book.read ? "Yes" : "No"}</p>
-            <button onclick="toggleReadStatus(${index})">Toggle Read Status</button>
-            <button onclick="removeBook(${index})">Remove Book</button>`;
-
-            libraryContainer.appendChild(bookCard);
-    });
+    libraryContainer.appendChild(bookCard);
+  });
 }
+
+function toggleReadStatus(index) {
+  myLibrary[index].toggleRead();
+  displayBooks();
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+document.getElementById('book-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').checked;
+
+  addBookToLibrary(title, author, pages, read);
+  document.getElementById('book-form').reset();
+});
